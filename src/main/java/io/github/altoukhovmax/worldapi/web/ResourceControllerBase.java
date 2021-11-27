f79ffe4ebf2e80b21b65a8455e0901b2a580f123
altoukhov-max <86380488+altoukhov-max@users.noreply.github.com>
2021-11-28 02:04:52 +0300

    Removed usage of 'sealed' from controller base

--- src/main/java/io/github/altoukhovmax/worldapi/web/ResourceControllerBase.java
+++ src/main/java/io/github/altoukhovmax/worldapi/web/ResourceControllerBase.java
@@ -7,8 +7,7 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public abstract sealed class ResourceControllerBase<E, D>
-        permits CityController, CountryController, LanguageController {
+public abstract class ResourceControllerBase<E, D> {
 
     private final Converter<E, D> entityConverter;
 
