628ae84c974a91c36e1ee802060ebe6906f201cf
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-01-25 05:38:56 +0300

    Added CityService

---
+++ src/main/java/com/altoukhov/globe/city/CityNotFoundException.java
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: Copyright 2022 Maxim Altoukhov
+// SPDX-License-Identifier: MIT
+
+package com.altoukhov.globe.city;
+
+import com.altoukhov.globe.common.ResourceNotFoundException;
+
+public class CityNotFoundException extends ResourceNotFoundException {
+
+    public CityNotFoundException() {
+        super();
+    }
+}
