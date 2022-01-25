321cd9cd404b151b969703d4e8c3d46f8e78a5fe
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-01-25 21:24:33 +0300

    Add LanguageService

---
+++ src/main/java/com/altoukhov/globe/language/LanguageNotFoundException.java
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: Copyright 2022 Maxim Altoukhov
+// SPDX-License-Identifier: MIT
+
+package com.altoukhov.globe.language;
+
+import com.altoukhov.globe.common.ResourceNotFoundException;
+
+public class LanguageNotFoundException extends ResourceNotFoundException {
+
+    public LanguageNotFoundException() {
+        super();
+    }
+}
